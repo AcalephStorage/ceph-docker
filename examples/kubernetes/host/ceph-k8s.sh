d47f4610edbf41ccf05a7b2a0775540b3b006c36
madz <madz@acale.ph>
2016-08-25 14:47:22 +0800

    add namespace to ceph-exporter resources.

--- examples/kubernetes/host/ceph-k8s.sh
+++ examples/kubernetes/host/ceph-k8s.sh
@@ -17,7 +17,6 @@ function config_create() {
 
   cd generator
   kubectl create secret generic ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring --namespace=ceph
-  kubectl create secret generic ceph-conf-combined --from-file=ceph.conf --from-file=ceph.client.admin.keyring --from-file=ceph.mon.keyring
   kubectl create secret generic ceph-bootstrap-rgw-keyring --from-file=ceph.keyring=ceph.rgw.keyring --namespace=ceph
   kubectl create secret generic ceph-bootstrap-mds-keyring --from-file=ceph.keyring=ceph.mds.keyring --namespace=ceph
   kubectl create secret generic ceph-bootstrap-osd-keyring --from-file=ceph.keyring=ceph.osd.keyring --namespace=ceph
@@ -137,7 +136,7 @@ function exp() {
 }
 
 function exp_get() {
-  kubectl get pods --selector="app=ceph-exporter"
+  kubectl get pods --selector="app=ceph-exporter" --namespace=ceph
 }
 
 function exp_create() {
