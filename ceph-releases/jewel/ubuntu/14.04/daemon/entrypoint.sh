c23e8bddd8e58103acd3f4a209f96660219b1376
dexter <dgenterone@acale.ph>
2016-11-25 15:01:55 +0800

    added get_config to start_mon so it does the symlinks on reboot

--- ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
@@ -213,6 +213,7 @@ function populate_kv {
 #######
 
 function start_mon {
+  get_config
   if [[ ! -n "$CEPH_PUBLIC_NETWORK" && ${NETWORK_AUTO_DETECT} -eq 0 ]]; then
     log "ERROR- CEPH_PUBLIC_NETWORK must be defined as the name of the network for the OSDs"
     exit 1
