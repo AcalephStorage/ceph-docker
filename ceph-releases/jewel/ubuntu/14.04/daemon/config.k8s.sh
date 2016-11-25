16a45aba393737827183728bfa1a858360b64625
dexter <dgenterone@acale.ph>
2016-11-25 14:20:53 +0800

    trying out simpler approach for monmap for k8s

--- ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
@@ -17,21 +17,7 @@ function get_mon_config {
   FSID=$(ceph-conf --lookup fsid -c /etc/ceph/ceph.conf)
 
   if [[ "$K8S_MONMAP_NAME_FROM" == "mon-name" ]]; then
-    # wait for all pods to be at least running or pending (since we're using kubectl exec')
-    expected_pods=$(kubectl get deployment --namespace=ceph -l daemon=mon --no-headers | wc -l)
-    while [[ "$expected_pods" != "$running_pods" ]]; do
-      running_pods=$(kubectl get pods --namespace=ceph -l daemon=mon --no-headers | grep [Running\|Pending] | wc -l)
-      sleep 1
-    done
-
-    # create the monname and ip of the pods
-    monpods=$(kubectl get pods --namespace ceph -l daemon=mon -o template --template="{{ range .items }}{{ .metadata.name }} {{ end }}")
-    MONMAP_ADD=""
-    for monpod in $monpods; do
-      monname=$(kubectl exec --namespace ceph $monpod -- bash -c 'echo ${MON_NAME:=${HOSTNAME}}')
-      monip=$(kubectl get pods --namespace ceph $monpod -o template --template="{{ .status.podIP }}")
-      MONMAP_ADD="$MONMAP_ADD --add $monname $monip "
-    done
+    MONMAP_ADD=$(echo --add "${MON_NAME} ${MON_IP} ")
   else
     # Get the ceph mon pods (name and IP) from the Kubernetes API. Formatted as a set of monmap params
     MONMAP_ADD=$(kubectl get pods --namespace=${CLUSTER} -l daemon=mon -o template --template="{{range .items}}--add {{.metadata.name}} {{.status.podIP}} {{end}}")
